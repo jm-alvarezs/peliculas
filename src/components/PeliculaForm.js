28727b33eb7ca5bf6025e1f3833770860444af35
Juan Manuel Alvarez <jm.alvarezs98@gmail.com2>
2019-08-08 15:24:00 -0500

    added PeliculaForm modifying new movie

--- src/components/PeliculaForm.js
+++ src/components/PeliculaForm.js
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import ReactDOM from "react-dom";
 import { setNombrePelicula, setDirectorPelicula, setCategoriaPelicula, setProtagonistas } from "../actions/peliculasActions";
 import { connect } from "react-redux";
 
 class PeliculaForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    handleSelect() {
+        let value = ReactDOM.findDOMNode(this.select).value;
+        this.props.setCategoriaPelicula(value);
+    }
+
     render() {
         return (
             <Container fluid={true} className="pt-3 pb-4">
                 <h2 className="mb-3">Pelicula</h2>
-                <Form.Control type="text" className="mb-2" placeholder="Nombre" value={this.props.pelicula.nombre} onChange={(e) => this.props.setNombrePelicula(e.target.value)} />
-                <Form.Control type="text" className="mb-2" placeholder="Director" value={this.props.pelicula.director} />
-                <Form.Control as="select" className="mb-2" ref={select => this.select = select} value={this.props.pelicula.categoria}>
+                <Form.Control type="text" className="mb-2" placeholder="Nombre" value={this.props.pelicula.nombre} onChange={e => this.props.setNombrePelicula(e.target.value)} />
+                <Form.Control type="text" className="mb-2" placeholder="Director" value={this.props.pelicula.director} onChange={e => this.props.setDirectorPelicula(e.target.value)} />
+                <Form.Control as="select" className="mb-2" ref={select => this.select = select} value={this.props.pelicula.categoria} onChange={this.handleSelect}>
                     <option>Terror</option>
                     <option>Amor</option>
                     <option>Acción</option>
                 </Form.Control>
-                <Form.Control type="text" placeholder="Protagonistas (separar por comas)" value={this.props.pelicula.protagonistas} />
+                <Form.Control type="text" placeholder="Protagonistas (separar por comas)" value={this.props.pelicula.protagonistas} onChange={e => this.props.setProtagonistas(e.target.value)} />
             </Container>
         );
     }
